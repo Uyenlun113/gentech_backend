ebccc5d67956d4370be02b3607a80f77e8ca1204
HiepnxDev <hiepnguyenxuan151@gmail.com>
2025-08-01 16:43:17 +0700

    update

--- src/bc-tienmat-tiengui-bank/bc-tienmat-tiengui-bank.controller.ts
+++ src/bc-tienmat-tiengui-bank/bc-tienmat-tiengui-bank.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Query, Get } from '@nestjs/common';
 import { BcTienmatTienguiBankService } from './bc-tienmat-tiengui-bank.service';
 import { CallStoreDto } from './dto/call-store.dto';
 
 @Controller('baocaovonbangtien')
 export class BcTienmatTienguiBankController {
-  constructor(private readonly service: BcTienmatTienguiBankService) {}
+  constructor(private readonly service: BcTienmatTienguiBankService) { }
 
-  @Post('tiengui')
-  async callStore(@Body() dto: CallStoreDto) {
+  @Get('tiengui')
+  async callStore(@Query() dto: CallStoreDto) {
     return this.service.callStore(dto);
   }
 }
--- src/bc-tienmat-tiengui-bank/bc-tienmat-tiengui-bank.service.ts
+++ src/bc-tienmat-tiengui-bank/bc-tienmat-tiengui-bank.service.ts
@@ -32,11 +32,11 @@ export class BcTienmatTienguiBankService {
           @store=@0, @tk=@1, @ngay_ct1=@2, @ngay_ct2=@3, @Ma_dvcs=@4`;
 
             const params = hasGopTk
-                ? [store, tk, ngay_ct1, ngay_ct2, gop_tk, ma_dvcs]
+                ? [store, tk, ngay_ct1, ngay_ct2, Number(gop_tk), ma_dvcs]
                 : [store, tk, ngay_ct1, ngay_ct2, ma_dvcs];
 
-            const result = await this.dataSource.query(query, params);
-            return result;
+            const data = await this.dataSource.query(query, params);
+            return data;
         } catch (e) {
             throw new Error('Create ph74 failed: ' + e.message);
         }

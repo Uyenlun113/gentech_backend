9982aa214c0295f4c68cd8fb1716746f2d01408a
HiepnxDev <hiepnguyenxuan151@gmail.com>
2025-08-08 10:18:17 +0700

    update

---
+++ src/bc-hang-ton-kho/dto/bc-hang-ton-kho.ts
@@ -0,0 +1,29 @@
+import { IsDateString, IsInt, IsOptional, IsString } from 'class-validator';
+
+export class CallReportDto {
+  @IsString()
+  store: string;
+
+  @IsString()
+  tk: string;
+
+  @IsDateString()
+  ngay_ct1: string; // YYYY-MM-DD or ISO
+
+  @IsDateString()
+  ngay_ct2: string;
+
+  @IsString()
+  gop_tk?: string; // optional
+
+  @IsString()
+  ma_dvcs: string;
+
+  @IsString()
+  @IsOptional()
+  OutputInsert?: string;
+
+  @IsInt()
+  @IsOptional()
+  stt_rec?: number;
+}
--- src/hoadon-phieuxuatkho/entity/ct81.entity.ts
+++ src/hoadon-phieuxuatkho/entity/ct81.entity.ts
@@ -43,4 +43,7 @@ export class Ct81 {
 
   @Column()
   tk_gv: string;
+  
+  @Column()
+  ma_ct: string;  
 }
